b08bcda9e8da60aaab23ee43dc1795776156e7fb
jspdown <harold.ozouf@tryon.io>
2015-03-14 15:23:38 +0200

    Fixed dropdown to allow them to open and close.Fixed dropdown title after a click on a dropdown-group. Improved demo.html.

--- src/dropdown/docs/controllers.js
+++ src/dropdown/docs/controllers.js
@@ -1,6 +1,17 @@
-var dropdownApp = angular.module('dropdownApp', ['angularify.semantic.dropdown']);
+
+angular
+  .module('dropdownApp', ['angularify.semantic.dropdown'])
+  .controller('RootCtrl', RootCtrl);
 
 function RootCtrl ($scope) {
     $scope.dropdown_model = 'item3';
+
+    $scope.dropdown_repeat_model = 'item1';
+    $scope.dropdown_items = [
+      'item1',
+      'item2',
+      'item3',
+      'item4'
+    ];
 }
 
