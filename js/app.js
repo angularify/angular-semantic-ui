288ecd2ac7e245b8b0321e2648b4a40877dc430b
Pierce Moore <me@prex.io>
2015-04-29 06:29:00 -0500

    Fixed spelling: raiting > rating

--- js/app.js
+++ js/app.js
@@ -4,5 +4,5 @@ var app = angular.module('App', ['angularify.semantic.accordion',
                                  'angularify.semantic.dimmer',
                                  'angularify.semantic.modal',
                                  'angularify.semantic.popup',
-                                 'angularify.semantic.raiting', 
+                                 'angularify.semantic.rating',
 	                             'angularify.semantic.sidebar']);
