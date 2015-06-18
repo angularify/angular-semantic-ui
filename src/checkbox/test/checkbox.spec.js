cd054234cd3329ca50bc291aa496a736f5926df7
Damien Szczyt <damien.szczyt@geniustrade.com>
2015-06-18 23:14:04 +0200

    Checkbox code refactoring

--- src/checkbox/test/checkbox.spec.js
+++ src/checkbox/test/checkbox.spec.js
@@ -1,9 +1,79 @@
 describe('checkbox', function () {
-    var $scope;
-
     beforeEach(module('angularify.semantic.checkbox'));
 
-    beforeEach(inject(function ($rootScope) {
-        $scope = $rootScope;
+    var $controller;
+    var $compile;
+    var $rootScope;
+
+    beforeEach(inject(function(_$controller_, _$compile_, _$rootScope_) {
+        $controller = _$controller_;
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
     }));
+
+    describe('directive', function() {
+        it('should require ng-model', function() {
+            expect(function() {
+                $compile('<checkbox>checkbox\'s label</checkbox>')($rootScope)
+            }).toThrow();
+        });
+
+        it('should compile when ng-model provided', function() {
+            var element = $compile('<checkbox ng-model="value">checkbox\'s label</checkbox>')($rootScope);
+            $rootScope.$digest();
+
+            expect(element.is('.ui.checkbox')).toBeTruthy()
+
+            expect(element.children().length).toEqual(2);
+            expect(angular.element(element.children()[0]).is('input:checkbox')).toBeTruthy();
+            expect(element.children('input').is('input:checkbox')).toBeTruthy();
+            expect(angular.element(element.children()[1]).is('label')).toBeTruthy();
+
+            expect(element.children('label').text()).toEqual('checkbox\'s label');
+            expect(element.find('input').is(':checked')).toBeFalsy();
+            expect($rootScope.value).toBeFalsy();
+
+            element.find('input').click();
+            expect(element.find('input').is(':checked')).toBeTruthy();
+            expect($rootScope.value).toBeTruthy();
+        });
+
+        it('should be checked when marked checked', function() {
+            var element = $compile('<checkbox ng-model="value" checked="true">checkbox\'s label</checkbox>')($rootScope);
+            $rootScope.$digest();
+            expect(element.find('input').is(':checked')).toBeTruthy();
+            expect($rootScope.value).toBeTruthy();
+        });
+
+        it('should be disabled when marked disabled', function() {
+            var element = $compile('<checkbox ng-model="value" disabled="true">checkbox\'s label</checkbox>')($rootScope);
+            $rootScope.$digest();
+            expect(element.find('input').is(':disabled')).toBeTruthy();
+        });
+
+        it('should be large when size is large', function() {
+            var element = $compile('<checkbox ng-model="value" size="large">checkbox\'s label</checkbox>')($rootScope);
+            $rootScope.$digest();
+            expect(element.is('.large')).toBeTruthy()
+        });
+
+        it('should be huge when size is huge', function() {
+            var element = $compile('<checkbox ng-model="value" size="huge">checkbox\'s label</checkbox>')($rootScope);
+            $rootScope.$digest();
+            expect(element.is('.huge')).toBeTruthy()
+        });
+
+        it('should be slider when type is slider', function() {
+            var element = $compile('<checkbox ng-model="value" type="slider">checkbox\'s label</checkbox>')($rootScope);
+            $rootScope.$digest();
+            expect(element.is('.slider')).toBeTruthy()
+        });
+
+        it('should be toggle when type is toggle', function() {
+            var element = $compile('<checkbox ng-model="value" type="toggle">checkbox\'s label</checkbox>')($rootScope);
+            $rootScope.$digest();
+            expect(element.is('.toggle')).toBeTruthy()
+        });
+
+    });
 });
