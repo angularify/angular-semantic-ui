6b7faec62c1a5c8f46c5dea32902a03cb3f438b2
Damien Szczyt <damien.szczyt@geniustrade.com>
2015-06-18 23:47:50 +0200

    We just require ngModel...

--- src/checkbox/checkbox.js
+++ src/checkbox/checkbox.js
@@ -32,7 +32,7 @@ angular.module('angularify.semantic.checkbox', [])
     },
     controllerAs: 'vm',
     bindToController: true,
-    require: [ 'checkbox', 'ngModel' ],
+    require: 'ngModel',
     template: "<div class=\"ui checkbox\" ng-class=\"vm.classes\">" +
       "<input type=\"checkbox\" ng-model=\"vm.ngModel\" ng-disabled=\"vm.disabled\"/>" +
       "<label ng-click=\"vm.toggle()\" ng-transclude></label>" +
