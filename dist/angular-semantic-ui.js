78e352d5b79e27c8ae1c582b1c9ee83eee5bb610
Damien Szczyt <damien.szczyt@geniustrade.com>
2015-07-08 23:44:45 +0200

    fix dist files too

--- dist/angular-semantic-ui.js
+++ dist/angular-semantic-ui.js
@@ -141,10 +141,10 @@ angular.module('angularify.semantic.checkbox', [])
     replace: true,
     transclude: true,
     scope: {
-      type: "@",
-      size: "@",
-      checked: "&?",
-      disabled: "&?",
+      type: '@',
+      size: '@',
+      checked: '&?',
+      disabled: '&?',
       ngModel: '=ngModel'
     },
     controller: function() {
@@ -171,10 +171,10 @@ angular.module('angularify.semantic.checkbox', [])
     controllerAs: 'vm',
     bindToController: true,
     require: 'ngModel',
-    template: "<div class=\"ui checkbox\" ng-class=\"vm.classes\">" +
-      "<input type=\"checkbox\" ng-model=\"vm.ngModel\" ng-disabled=\"vm.disabled()\"/>" +
-      "<label ng-click=\"vm.toggle()\" ng-transclude></label>" +
-      "</div>",
+    template: '<div class="ui checkbox" ng-class="vm.classes">' +
+      '<input type="checkbox" ng-model="vm.ngModel" ng-disabled="vm.disabled()"/>' +
+      '<label ng-click="vm.toggle()" ng-transclude></label>' +
+      '</div>',
     link: function() { }
   };
 });
