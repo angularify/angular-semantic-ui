00906843b04e95a10ae53196f9d07a09d2b02a1c
Damien Szczyt <damien.szczyt@geniustrade.com>
2015-07-08 23:44:06 +0200

    fix dist files too

--- dist/angular-semantic-ui.js
+++ dist/angular-semantic-ui.js
@@ -150,9 +150,10 @@ angular.module('angularify.semantic.checkbox', [])
     controller: function() {
       var vm = this;
 
-      if(vm.checked) {
-        vm.ngModel = true;
-      }
+      // TODO: assert this is usefull ?
+      // if(angular.isUndefined(vm.ngModel)) { vm.ngModel = !!vm.ngModel; }
+
+      if(angular.isFunction(vm.checked)) { vm.ngModel = !!vm.checked(); }
 
       vm.classes = {
         slider: vm.type == 'slider',
@@ -162,13 +163,16 @@ angular.module('angularify.semantic.checkbox', [])
         huge: vm.size == 'huge'
       };
 
-      vm.toggle = function() { vm.ngModel = !vm.ngModel }
+      vm.toggle = function() {
+        if(angular.isFunction(vm.disabled) && vm.disabled()) return;
+        vm.ngModel = !vm.ngModel;
+      }
     },
     controllerAs: 'vm',
     bindToController: true,
-    require: [ 'checkbox', 'ngModel' ],
+    require: 'ngModel',
     template: "<div class=\"ui checkbox\" ng-class=\"vm.classes\">" +
-      "<input type=\"checkbox\" ng-model=\"vm.ngModel\" ng-checked=\"vm.checked\" ng-disabled=\"vm.disabled\"/>" +
+      "<input type=\"checkbox\" ng-model=\"vm.ngModel\" ng-disabled=\"vm.disabled()\"/>" +
       "<label ng-click=\"vm.toggle()\" ng-transclude></label>" +
       "</div>",
     link: function() { }
