62da6ec283f39e29eb340f0faecbeb1258461a61
Damien Szczyt <damien.szczyt@geniustrade.com>
2015-07-27 07:48:53 +0200

    remove classes from scope on transcluded element

--- dist/angular-semantic-ui.js
+++ dist/angular-semantic-ui.js
@@ -163,7 +163,7 @@ angular.module('angularify.semantic.checkbox', [])
     controllerAs: 'vm',
     bindToController: true,
     require: 'ngModel',
-    template: '<div class="ui checkbox" ng-class="vm.classes">' +
+    template: '<div class="ui checkbox">' +
       '<input type="checkbox" ng-model="vm.ngModel" ng-disabled="vm.disabled()"/>' +
       '<label ng-click="vm.toggle()" ng-transclude></label>' +
       '</div>',
