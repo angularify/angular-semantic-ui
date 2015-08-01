db00b02b2336524b254a23625425ee147c965ac4
Damien Szczyt <damien.szczyt@geniustrade.com>
2015-08-01 14:57:08 +0200

    checkbox refactoring

--- dist/angular-semantic-ui.js
+++ dist/angular-semantic-ui.js
@@ -586,111 +586,62 @@ function sidebarLink() {
 'use strict';
 
 angular.module('angularify.semantic.rating', [])
-
 .directive('rating', function(){
-    return {
-        restrict: "E",
-        replace: true,
-        transclude: true,
-        scope: {
-            id: "@",
-            size: "@",
-            type: "@",
-            model : '=ngModel'
-        },
-        template: '<div class={{div_class}}>' + 
-                     '<i id="{{id + 1}}" class="{{icon_class}}" ng-click="click(1)" ng-mouseenter="mouse_enter(1)" ng-mouseleave="mouse_leave(1)"></i>' +
-                     '<i id="{{id + 2}}" class="{{icon_class}}" ng-click="click(2)" ng-mouseenter="mouse_enter(2)" ng-mouseleave="mouse_leave(2)"></i>' +
-                     '<i id="{{id + 3}}" class="{{icon_class}}" ng-click="click(3)" ng-mouseenter="mouse_enter(3)" ng-mouseleave="mouse_leave(3)"></i>' +
-                     '<i id="{{id + 4}}" class="{{icon_class}}" ng-click="click(4)" ng-mouseenter="mouse_enter(4)" ng-mouseleave="mouse_leave(4)"></i>' +
-                     '<i id="{{id + 5}}" class="{{icon_class}}" ng-click="click(5)" ng-mouseenter="mouse_enter(5)" ng-mouseleave="mouse_leave(5)"></i>' +
-                   '</div>',
-        link: function(scope, element, attrs){
-            if (scope.model == undefined)
-                scope.model = 0;
+  return {
+    restrict: "E",
+    replace: true,
+    scope: {
+      ngModel : '=',
+      max: '=',
+      onRate: '&?',
+      interactive: '=',
+      clearable: '='
+    },
+    controller: function($scope) {
+      var vm = this;
 
-            if (scope.model < 1 && scope.model > 5)
-                scope.model = 0;
+      if(!vm.max) vm.max = 5;
+      if(!+vm.rate) vm.rate = 0;
+      if(vm.rate > vm.max ) vm.rate = vm.max;
+      if(vm.interactive == undefined) vm.interactive = true;
 
-            // is rating already checked
-            var checked = false;
-            
-            //
-            // Set up icon type
-            //
-            if (scope.type == undefined)
-                scope.type = 'star';
-
-            //
-            // Set up size
-            //
-            if (scope.size == undefined) 
-                scope.div_class = 'ui rating ' + scope.type;
-            else if (scope.size == 'small')
-                scope.div_class = 'ui small ' + scope.type + ' rating';
-            else if (scope.size == 'large')
-                scope.div_class = 'ui large ' + scope.type + ' rating';
-            else if (scope.size == 'huge')
-                scope.div_class = 'ui huge ' + scope.type + ' rating';
-
-            //
-            // set up icon class
-            //
-            scope.icon_class = 'icon';
-
-            //
-            // Handle mouse enter
-            //
-            scope.mouse_enter = function(icon_index){
-                if (checked == true)
-                    return;
-
-                var i = 1;
-                for (i; i <= icon_index; i++){
-                    document.getElementById(scope.id + i).className = 'icon active';
-                }
-
-                return;
-            };
-
-            //
-            // Handle mouse leave
-            //
-            scope.mouse_leave = function(icon_index){
-                if (checked == true)
-                    return;
-
-                var i = 1;
-                for (i; i <= 5; i++){
-                    document.getElementById(scope.id + i).className = 'icon';
-                }
-
-                return;
-            };
-
-            //
-            // Handle click
-            //
-            scope.click = function(icon_index, mode){
-                var i = 1;
-                for (i; i <= icon_index; i++){
-                    document.getElementById(scope.id + i).className = 'icon active';
-                }
-
-                if (icon_index !== 0)
-                    checked = true;
-
-                return;
-            };
-
-            //
-            // Watch for model
-            //
-            scope.$watch('model', function(val){
-                scope.click(val);
-            });
+      vm.setRate = function(rate) {
+        if (vm.clearable && rate == +vm.ngModel) {
+          vm.ngModel = 0;
+        } else {
+          vm.ngModel = rate;
+          if (angular.isFunction(vm.onRate)) vm.onRate({ rate: rate });
         }
-    };
+      };
+    },
+    controllerAs: 'vm',
+    bindToController: true,
+    require: 'ngModel',
+    template: '<div class="ui rating" ng-switch="vm.interactive">' +
+                '<i ng-switch-when="true" ' +
+                  'class="icon" ' +
+                  'ng-class="{ active: vm.current >= rate }" ' +
+                  'ng-click="vm.setRate(rate)" ' +
+                  'ng-mouseenter="vm.current = rate" ' +
+                  'ng-mouseleave="vm.current = +vm.ngModel" ' +
+                  'ng-repeat="rate in vm.range track by $index"' +
+                '></i>' +
+                '<i ng-switch-when="false" ' +
+                  'class="icon" ' +
+                  'ng-class="{ active: vm.current >= rate }" ' +
+                  'ng-repeat="rate in vm.range track by $index"' +
+                '></i>' +
+              '</div>',
+    link: function($scope, iElement, iAttrs) {
+      $scope.$watch('vm.max', function (value) {
+        $scope.vm.range = Array.apply(null, Array(+value)).map(function (_, i) {return i+1;});
+      });
+
+      $scope.$watch('vm.ngModel', function(value) {
+        $scope.vm.current = value;
+      });
+    }
+  };
 });
 
 /* globals _:false */
--- src/rating/docs/controllers.js
+++ src/rating/docs/controllers.js
@@ -1,5 +1,12 @@
-var ratingApp = angular.module('ratingApp', ['angularify.semantic.rating']);
+angular.module('ratingApp', ['angularify.semantic.rating'])
+.controller('RootCtrl', function($scope) {
+  $scope.rate = 3;
+  $scope.rated = function(rate) {
+    alert('Rate is ' + rate);
+  };
 
-function RootCtrl ($scope) {
-    // $scope.rating = 1;
-}
+  $scope.clearable_rate = 2;
+
+  $scope.fixed_rate = 4;
+});
+
